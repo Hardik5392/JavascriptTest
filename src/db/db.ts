202c4579520726ec58b4e65c95b14eaf9feac489
hardikt <hardik@albiorixtech.com>
2024-03-22 18:47:39 +0530

    Remove string error.

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connect() {
   try {
-    mongoose.connect(process.env.DB_URI);
+    mongoose.connect(process.env.DB_URI || "");
   } catch (error) {
     console.log("Something goes wrong!");
     console.log(error);
